0a0d0555351efd5a075b0c9a1766b6bb968466f3
Alice (KHisamutdinovEA) <hev1966@yandex.ru>
2021-03-01 16:53:40 +0300

    code: Add funct Max

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <stdlib.h>
 using namespace std;
 
 int main()
@@ -7,7 +7,11 @@ int main()
     cout << "Enter A and B: " << endl;
     int a, b;
     cin >> a >> b;
-    cout <<  "Sum = " << a+b << '\n' <<  "Div = " << a-b << '\n' <<  "A*B" << a*b << '\n'  << "A/B" << a/b << '\n';
+    cout <<  "Sum = " << a+b << '\n'
+         <<  "Div = " << a-b << '\n'
+         <<  "A*B" << a*b << '\n'
+         << "A/B" << a/b << '\n'
+         << "Max = " <<max(a,b)<< '\n';
 
     return 0;
 }
